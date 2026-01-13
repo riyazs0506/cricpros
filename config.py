57bf338c11674e182ad6257e0ce3da1ac185cc88
riyazs0506 <mohammedriyazs0506@gmail.com>
2026-01-13 22:16:52 +0530

    first commit

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
     )
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
