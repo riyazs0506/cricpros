00fe5b593eff1f4dcb9036fdecb9128780d629af
riyazs0506 <mohammedriyazs0506@gmail.com>
2026-01-13 22:15:57 +0530

    first commit

--- config.py
+++ config.py
@@ -7,10 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key_here")
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-       # "mysql+pymysql://root:oMjTTmqBjCxuBXiEGUmQyngfKnaekmVt@centerbeam.proxy.rlwy.net:58790/railway"
-        # "mysql+pymysql://root:123456@localhost/Cricpros"
-        "mysql+pymysql://avnadmin:REDACTED-AIVEN-PASSWORD@cricpros-cricpro.l.aivencloud.com:20934/defaultdb"
+        "DATABASE_URL"
     )
 
 
