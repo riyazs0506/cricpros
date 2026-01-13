bbef4cc62dd08acc9bfe6b657cfbb31a46197a82
riyazs0506 <mohammedriyazs0506@gmail.com>
2026-01-13 22:18:59 +0530

    first commit

--- config.py
+++ config.py
@@ -4,12 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key_here")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
